77c9e2ed9842eac0f0b81d222ed8ec37f7007748
raghugg <guggilam.raghu@gmail.com>
2026-05-06 00:15:37 -0400

    Add postgres password

--- backend/server.js
+++ backend/server.js
@@ -8,7 +8,7 @@ const pool = new Pool({
     user: 'postgres',
     host: 'localhost',
     database: 'leaderboard',
-    password: '',
+    password: process.env.DB_PASSWORD,
     port: 5432,
 });
 
